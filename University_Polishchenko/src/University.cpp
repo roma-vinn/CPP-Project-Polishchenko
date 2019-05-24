8899f8c515ef01f8743c775281791419a53c06b7
Roman Polishchenko <roma.vinn@gmail.com>
2019-05-24 23:55:55 +0300

    Modified console mode

--- University_Polishchenko/src/University.cpp
+++ University_Polishchenko/src/University.cpp
@@ -78,16 +78,16 @@ int _read_from_console(vector<Teacher*> & all_teachers,
             for (int i = 1; i < count + 1; ++i) {
                 
                 int age = 0, student_number = 0;
-                string name = "", degree="None";
+                string t_name = "", degree="None";
                 
                 cout << "Teacher " << i << ":\n";
                 cout << "Name: ";
-                getline(cin, name, '\n');
+                getline(cin, t_name, '\n');
                 cout << "Age: ";
                 age = read_int();
                 cout << "Degree: ";
                 getline(cin, degree, '\n');
-                Teacher* t = new Teacher(name, age, degree);
+                Teacher* t = new Teacher(t_name, age, degree);
                 cout << "Enter the number of this teacher's students: ";
                 student_number = read_int();
                 if (student_number < 0) {
@@ -95,13 +95,13 @@ int _read_from_console(vector<Teacher*> & all_teachers,
                     ". Should be not less than 0.\n";
                     return 1;
                 } else {
-                    string name = "";
+                    string s_name = "";
                     int age = 0, sub_number = 0;
                     RecordBook rb;
                     for (int j = 1; j < student_number + 1; ++j) {
                         cout << "Student " << j << ":\n";
                         cout << "Name: ";
-                        getline(cin, name, '\n');
+                        getline(cin, s_name, '\n');
                         cout << "Age: ";
                         age = read_int();
                         cout << "Number of subjects: ";
@@ -120,9 +120,9 @@ int _read_from_console(vector<Teacher*> & all_teachers,
                                 rb.updateSubject(sub_name, grade_point);
                             }
                         }
-                        Student student(name, age, rb);
+                        Student student(s_name, age, t_name, rb);
                         t->addStudent(student);
-                        all_students.push_back(new Student(name, age, rb));
+                        all_students.push_back(new Student(s_name, age, t_name, rb));
                     }
                 }
                 all_teachers.push_back(t);
@@ -138,11 +138,13 @@ int console_mode() {
     vector<Worker*> all_workers;
     vector<Student*> all_students;
     
+    // read data from console
     if (_read_from_console(all_teachers, all_workers, all_students)) {
         cout << "Incorrect input.\n";
         return 1;
     }
     
+    // show all existent data
     cout << "All workers:\n";
     _showVector(all_workers);
     
@@ -152,8 +154,9 @@ int console_mode() {
     cout << "All students:\n";
     _showVector(all_students);
     
-    bool flag = true;
+    bool flag = true, found = false;
     int choice = 1;
+    string teacher_name = "";
     while (flag) {
         cout << "Choose and type the number of the action below:\n";
         cout << "1. Show all workers.\n";
@@ -175,8 +178,65 @@ int console_mode() {
                 _showVector(all_workers);
                 break;
                 
+            case 2:
+                cout << "All teachers:\n";
+                _showVector(all_teachers);
+                break;
             
+            case 3:
+                cout << "All students:\n";
+                _showVector(all_students);
+                break;
+                
+            case 4:
+                cout << "Name of the teacher: ";
+                getline(cin, teacher_name, '\n');
+                
+                found = false;
+                for (vector<Teacher*>::const_iterator it = all_teachers.begin(),
+                     end = all_teachers.end(); it != end; ++it) {
+                    if ((*it)->getName() == teacher_name) {
+                        (*it)->showStudents();
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    cout << "Hasn't found such teacher. Check if the name is correct.\n";
+                }
+                break;
+            
+            case 5:
+                
+                break;
+                
+            case 6:
+                
+                break;
+                
+            case 7:
+                
+                break;
+                
+            case 8:
+                
+                break;
+                
+            case 9:
+                
+                break;
+                
+            case 10:
+                
+                break;
+                
+            case 0:
+                flag = false;
+                cout << "Returning to main menu.\n";
+                break;
+                
             default:
+                cout << "Incorrect command: should be number [0, 1, .., 10].\n";
                 break;
         }
         
